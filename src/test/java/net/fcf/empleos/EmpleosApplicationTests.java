9c4f7026b7a09998a5b8648ce1ccdfca79f0c46c
FrancisCastillo24 <162831161+FrancisCastillo24@users.noreply.github.com>
2025-06-23 14:56:34 +0200

    Creación controlador Home

---
+++ src/test/java/net/fcf/empleos/EmpleosApplicationTests.java
@@ -0,0 +1,13 @@
+package net.fcf.empleos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
